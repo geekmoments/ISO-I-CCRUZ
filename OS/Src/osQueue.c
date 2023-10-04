ca2628b0ce7a912ef2272c4bdb59241aa8257983
geekmoments <cesar@geek-moments.com>
2023-10-03 23:39:20 -0500

    agregando .h y .c de API, estructuras de semaforo binario y contador, mas inicializacion

---
+++ OS/Src/osQueue.c
@@ -0,0 +1,9 @@
+/*
+ * osQueue.c
+ *
+ *  Created on: Oct 3, 2023
+ *      Author: cesarcruz
+ */
+
+
+#include "osQueue.h"
---
+++ OS/Src/osSemaphore.c
@@ -0,0 +1,19 @@
+/*
+ * osSemaphore.h
+ *
+ *  Created on: Oct 3, 2023
+ *      Author: cesarcruz
+ */
+#include <osSemaphore.h>
+
+
+void osSemaphoreInit(osSemaphore* semaphore, bool isBinary, uint8_t maxCount) {
+    semaphore->isBinary = isBinary;
+    if (isBinary) {
+        semaphore->data.binary.status = false;
+        semaphore->data.binary.taskSemaphore = NULL;
+    } else {
+        semaphore->data.counter.counter = maxCount;
+        semaphore->data.counter.max = maxCount;
+    }
+}
